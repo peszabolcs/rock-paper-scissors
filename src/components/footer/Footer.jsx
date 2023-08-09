869c16bbcf893d02567499ad14005708d63259f8
peszabolcs <perjesidev@gmail.com>
2023-08-10 01:45:46 +0200

    Add footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer">
+        <div id="footer">Copyright © Perjési Szabolcs</div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
